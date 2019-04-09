094b9bca68f8f3a429672150fe90f199cb724657
martin-raven <martinsiby@gmail.com>
2019-04-10 00:02:06 +0530

    Alpha Program completed

--- Test.py
+++ Test.py
@@ -5,7 +5,9 @@ from googleapiclient import discovery
 					# Static Variables
 spreadsheet_id="1A3Ph6mfzKO-Aollw8zCx86-PwCoI-wMJVf5hTtEb3bs"
 range_one="Course"
-range_two="module"
+range_two="Module"
+range_three="Lesson"
+range_four="media"
 
 
 # Handling Network retrivals
@@ -37,10 +39,115 @@ def retrive(range):
 		print("Exception in retrival : ",e)
 		quit()
 
-# Function to parse modules into Courses
+
+# Function to parse Media into Lessons
+def ParseMedias(range,Courses):
+	MediasData=retrive(range)
+	# print(modulesData)
+	Medias={}
+	for Item in MediasData[1:]:
+		# print(Item)
+		Media={}
+		try:
+			for key,entry in zip(MediasData[0],Item):
+				if entry!="":
+					Media[key]=entry.rstrip('\n')
+					# print(entry,key)
+		except Exception as e:
+			print("Unable to add ",Item," because ",e)
+		if Media!={}:
+			Medias[Media["UID"].rstrip('\n')]=Media
+	# print(Medias)
+	UnusedMedias=list(Medias.keys())
+	for Course in Courses:
+		for Module in Course["modules"]:
+			# print(Module)
+			for Lesson in Module["lessons"]:
+				Lesson["medias"]=Lesson["medias"].split(",")
+				for LessonMedia in Lesson["medias"]:
+					# print(LessonMedia)
+					try:
+						if(LessonMedia=="ram_bhajan_1_17_Cs3_audio"):
+							print("Debug",Lesson["medias"],LessonMedia)
+						Lesson["medias"][Lesson["medias"].index(LessonMedia)]=Medias[LessonMedia]
+						UnusedMedias.remove(LessonMedia)
+					except Exception as e:
+						print(e," media mensioned in ",Lesson," is missing")
+	print(Courses)
+	if UnusedMedias!=[]:
+		print("The following medias have no parent: ",UnusedMedias)
+	return Courses
+
+# Function to parse Lessons into Modules
+def ParseLessons(range,Courses):
+	LessonsData=retrive(range)
+	# print(modulesData)
+	Lessons={}
+	for Item in LessonsData[1:]:
+		# print(Item)
+		Lesson={}
+		try:
+			for key,entry in zip(LessonsData[0],Item):
+				if entry!="":
+					Lesson[key]=entry.rstrip('\n')
+					# print(entry,key)
+		except Exception as e:
+			print("Unable to add ",Item," because ",e)
+		if Lesson!={}:
+			Lessons[Lesson["UID"]]=Lesson
+	# print(Lessons)
+	UnusedLessons=list(Lessons.keys())
+	for Course in Courses:
+		for Module in Course["modules"]:
+			# print(Module)
+			Module["lessons"]=Module["lessons"].split(",")
+			for ModuleLesson in Module["lessons"]:
+				print(ModuleLesson)
+				try:
+					Module["lessons"][Module["lessons"].index(ModuleLesson)]=Lessons[ModuleLesson]
+					UnusedLessons.remove(ModuleLesson)
+				except Exception as e:
+					print(e," lesson is missing !!")
+	# print(Courses)
+	if UnusedLessons!=[]:
+		print("The following lessons have no parent: ",UnusedLessons)
+	return Courses
+
+
+
+# Function to parse Modules into Courses
 def ParseModules(range,Courses):
 	modulesData=retrive(range)
-	print(modulesData)
+	# print(modulesData)
+	Modules={}
+	for Item in modulesData[1:]:
+		# print(Item)
+		Module={}
+		try:
+			for key,entry in zip(modulesData[0],Item):
+				if entry!="":
+					Module[key]=entry.rstrip('\n')
+					# print(entry,key)
+		except Exception as e:
+			print("Unable to add ",Item," because ",e)
+		if Module!={}:
+			Modules[Module["UID"]]=Module
+	# print(Modules)
+	UnusedModules=list(Modules.keys())
+	for Course in Courses:
+		Course["modules"]=Course["modules"].split(',')
+		for CourseModule in Course["modules"]:
+			# print(CourseModule)
+			try:
+				Course["modules"][Course["modules"].index(CourseModule)]=Modules[CourseModule]
+				UnusedModules.remove(CourseModule)
+			except Exception as e:
+				print(e," module is missing !!")
+	# print(Courses)
+	if UnusedModules!=[]:
+		print("The following modules have no parent: ",UnusedModules)
+	return Courses
+
 
 					# Function to parse Courses
 def ParseCourse(range):
@@ -66,9 +173,15 @@ def main():
 		# print(CourseData)
 						# Parsing the data into a JSON
 		Courses=ParseCourse(range_one)
-		print(Courses)
+		# print(Courses)
 						# Parsing module data into Courses
 		Courses=ParseModules(range_two,Courses)
+						# Parsing lessons data into Modules
+		Courses=ParseLessons(range_three,Courses)
+		# print(Courses)
+						# Parsing medias data into Lessons
+		Courses=ParseMedias(range_four,Courses)
+
 	
 if __name__ == '__main__':
     main()
