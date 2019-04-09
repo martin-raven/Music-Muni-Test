ace6b6741696e1e6c7ace54544884e3595317519
martin-raven <martinsiby@gmail.com>
2019-04-09 21:52:51 +0530

    Seperated all as individual functions

--- Test.py
+++ Test.py
@@ -2,34 +2,49 @@ import os
 import pickle
 from googleapiclient import discovery
 
-# Static Variables
+					# Static Variables
 spreadsheet_id="1A3Ph6mfzKO-Aollw8zCx86-PwCoI-wMJVf5hTtEb3bs"
 range_one="Course"
+range_two="module"
 
-# Initialize credentials
-credentials = None
-if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            credentials = pickle.load(token)
-else:
-	print("Please run the InitialRun.py before this file")
-	quit()
 
-# Main Service initialized 
-service = discovery.build('sheets', 'v4', credentials=credentials)
+# Handling Network retrivals
+def retrive(range):
+	try:
 
-try:
-	result = service.spreadsheets().values().get(
-	    spreadsheetId=spreadsheet_id, range=range_one).execute()
-	# Getting the values into the list
-	if "values" in result and result["values"]!=[]:
-		CourseData=result["values"][1:]
-	# Incase the values is empty or error occurs while requesting
-	else:
-		print("No Courses found")
+					# Initialize credentials
+		credentials = None
+		if os.path.exists('token.pickle'):
+		        with open('token.pickle', 'rb') as token:
+		            credentials = pickle.load(token)
+		else:
+			print("Please run the InitialRun.py before this file")
+			quit()
+
+						# Main Service initialized 
+		service = discovery.build('sheets', 'v4', credentials=credentials)
+		result = service.spreadsheets().values().get(
+		    spreadsheetId=spreadsheet_id, range=range).execute()
+						# Getting the values into the list
+		if "values" in result and result["values"]!=[]:
+			Data=result["values"][1:]
+						# Incase the values is empty or error occurs while requesting
+		else:
+			print("No Courses found")
+			quit()
+		return Data
+	except Exception as e:
+		print("Exception in retrival : ",e)
 		quit()
-	# print(CourseData)
-	# Parsing the data into a JSON
+
+# Function to parse modules into Courses
+def ParseModules(range,Courses):
+	modulesData=retrive(range)
+	print(modulesData)
+
+					# Function to parse Courses
+def ParseCourse(range):
+	CourseData=retrive(range)
 	Courses=[]
 	for Item in CourseData[1:]:
 		# print(Item)
@@ -38,12 +53,22 @@ try:
 			for key,entry in zip(CourseData[0],Item):
 				if entry!="":
 					Course[key]=entry
-					print(entry,key)
+					# print(entry,key)
 		except Exception as e:
-			print(e)
+			print("Unable to add ",Item," because ",e)
 		if Course!={}:
 			Courses.append(Course)
-	print(Courses)
-except Exception as e:
-	print(e)
-	quit()
+	return Courses 
+					#Main Function 
+def main():
+
+
+		# print(CourseData)
+						# Parsing the data into a JSON
+		Courses=ParseCourse(range_one)
+		print(Courses)
+						# Parsing module data into Courses
+		Courses=ParseModules(range_two,Courses)
+	
+if __name__ == '__main__':
+    main()
